97c729bdd47a2879941794e9ab62f70b20389a9f
= <=>
2018-09-21 14:35:02 -0300

    iginore conexao

--- models/conexao.php
+++ models/conexao.php
@@ -9,20 +9,7 @@ class Conexao {
         //$this->conectar();
     }
 	
-	/* exemplo1 */
-//    public function conectar() {
-//        $host = 'localhost';
-//        $banco = 'codigo_barras';
-//        $user = 'root';
-//        $senha = '';
-//        try {
-//            $this->con = new PDO("mysql:host=" . $host . ";dbname=" . $banco . "", "" . $user . "", "" . $senha . "");
-//        } catch (Exception $ex) {
-//            print_r($ex);
-//        }
-//        return $this->con;
-//    }
-	/* exemplo2 */
+    /* configuração */
     public static function getInstance() {
         
         $host = 'localhost';
@@ -39,3 +26,32 @@ class Conexao {
     }
 
 }
+
+/*
+class Conexao {
+
+    private $con;
+    public static $instance;
+
+    public function __construct() {
+        //$this->conectar();
+    }
+	
+    /* configuração * /
+    public static function getInstance() {
+        
+        $host = 'localhost';
+        $banco = 'codigo_barras';
+        $user = 'root';
+        $senha = '';
+        
+        if (!isset(self::$instance)) {
+            self::$instance = new PDO('mysql:host='.$host.';dbname='.$banco.'', $user, $senha, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+        }
+        return self::$instance;
+    }
+
+}
+*/
