2665f07b3e6948fe2e13ae4063c2a0a4e78618a1
= <=>
2019-10-08 20:29:41 -0300

    atualizacao view produtos

--- views/produtos/produtos.php
+++ views/produtos/produtos.php
@@ -67,6 +67,8 @@ $url .= '/codigo/controllers/cadastroController.php';
 
 </style>
 
+<iframe width="100%" height="760" src="https://www.youtube-nocookie.com/embed/TnHSL9bytos?controls=0&amp;start=2" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 <section id="main">
     <div class="main">
         <p class="return">           
